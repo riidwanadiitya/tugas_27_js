1cc5b9385463abb3334e9792ed3dce2dd23b470d
Ridwan <71011389+riidwanadiitya@users.noreply.github.com>
2021-01-04 09:42:38 +0700

    TUGAS 27 JS

---
+++ tugas27.js
@@ -0,0 +1,11 @@
+var a = "No ";
+var b = "rumah saya ";
+var c = 30;
+var d = "A"
+
+console.log(a + b + String(c) + d);
+
+
+var m = "Jalan Pangeran Tirtayasa No ";
+var n = 70;
+console.log(m + String(n))
